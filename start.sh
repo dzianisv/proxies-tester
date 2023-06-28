8278311367911988f123876a04e3a8714bd42513
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2023-06-28 18:57:38 +0300

    Add start script

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -eu
+
+if ! command -v node > /dev/null; then
+    brew install -y node
+fi
+
+if ! command -v npm > /dev/null; then
+    brew install -y npm
+fi
+
+if [ ! -d node_modules ]; then
+    npm install
+fi
+
+if [  -f proxies.txt ]; then
+    echo 'Getting proxies list: proxies.txt'
+    node_modules/.bin/proxy-lists getProxies
+fi
+
+echo 'Validating proxies, validated proxies will be stored at validated.txt'
+node index.js proxies.txt
