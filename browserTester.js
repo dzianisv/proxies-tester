f72155c3b629265c1a2cbd15f6674c93bb886c86
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2023-07-04 17:09:13 +0300

    Run headless mode

--- browserTester.js
+++ browserTester.js
@@ -1,9 +1,18 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-
-async function getCaptchaMessage(page, selector, timeout) {
-    const frame = await page.waitForSelector('iframe[src=*"https://geo.captcha-delivery.com"]'s, {timeout});
-    return await frame.contentFrame().$(selector);
+/*
+    let iframe = document.querySelector('iframe[src^="https://geo.captcha-delivery.com"]');
+    let element = iframe.contentWindow.document.body.querySelector('.captcha__human__title');
+    console.log(element.innerText);
+*/
+// <div class="captcha__human__title" tabindex="0">We want to make sure it is actually you we are dealing with and not a robot.</div>
+async function isGeoCaptchaDeliveryThere(page, timeout) {
+    let handle = await page.waitForSelector('iframe[src^="https://geo.captcha-delivery.com"]', {timeout});
+    return await handle.contentFrame();
+    // TODO: I didn't figure out how to get element from inside the frame
+    // const frame = await frameHandle.contentFrame();
+    // console.log(__filename, "found geo.captcha-delivery.com iframe");
+    // return await frame.waitForSelector('.captcha__human__title');
 }
 
 async function waitForReadyPage(page, timeout) {
@@ -13,8 +22,7 @@ async function waitForReadyPage(page, timeout) {
             const elements = Array.from(document.querySelectorAll('td'));
             return elements.find(e => e.innerText.includes('You have been temporarily blocked to prevent unauthorized use. you have reached the maximum allowed connections, please try later.'));
         }, { timeout }),
-        // <div class="captcha__human__title" tabindex="0">We want to make sure it is actually you we are dealing with and not a robot.</div>
-        getCaptchaMessage(page, '.captcha__human__title', timeout)
+        isGeoCaptchaDeliveryThere(page, timeout)
     ]);
 
     let message;
@@ -39,7 +47,7 @@ async function waitForReadyPage(page, timeout) {
 async function testProxy(proxy, timeout = 30000, test_url = "", selector = "") {
     console.log(__filename, "testing", proxy);
     puppeteer.use(StealthPlugin());
-    const browser = await puppeteer.launch({ headless: false, args: [`--proxy-server=${proxy.protocol}://${proxy.ip}:${proxy.port}`] });
+    const browser = await puppeteer.launch({ headless: true, args: [`--proxy-server=${proxy.protocol}://${proxy.ip}:${proxy.port}`] });
     const page = await browser.newPage();
 
     // page load timeout
