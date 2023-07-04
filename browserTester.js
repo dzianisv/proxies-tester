5dc7e527af84af8149f281db782e38d2e3f4e79d
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2023-07-04 16:35:18 +0300

    Expect captcha

--- browserTester.js
+++ browserTester.js
@@ -1,51 +1,10 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 
-// <div class="captcha__human__title" tabindex="0">We want to make sure it is actually you we are dealing with and not a robot.</div>
-const waitForElementInFrame = async (page, selector, timeout) => {
-    return new Promise((resolve, reject) => {
-        let captchaFrame;
-        let captchaFrameAttached = false;
-
-        page.on('framenavigated', frame => {
-            if (frame.url().startsWith('https://geo.captcha-delivery.com/captcha/')) {
-                console.log(__filename, 'attached frame', frame.url());
-                captchaFrame = frame;
-                captchaFrameAttached = true;
-            }
-        });
-
-        page.on('framedetached', frame => {
-            if (frame === captchaFrame) {
-                captchaFrameAttached = false;
-            }
-        });
-
-        const checkInterval = setInterval(async () => {
-            if (captchaFrameAttached) {
-                clearInterval(checkInterval);
-                console.log(__filename, "waiting for captcha message", selector);
-                try {
-                    const element = await captchaFrame.waitForFunction(() => {
-                        const elements = Array.from(document.querySelectorAll(selector));
-                        console.log(__filename, "found elements", elements);
-                        return elements.find(e => e.innerText.includes('We want to make sure it is actually you we are dealing with and not a robot.'));
-                    }, { timeout });
-                    resolve(element);
-                } catch (error) {
-                    console.log(__filename, "failed to locate captcha message", error);
-                    reject(error);
-                }
-            }
-        }, 1000);
-
-        setTimeout(() => {
-            clearInterval(checkInterval);
-            reject('Timeout');
-        }, timeout);
-    });
-};
-
+async function getCaptchaMessage(page, selector, timeout) {
+    const frame = await page.waitForSelector('iframe[src=*"https://geo.captcha-delivery.com"]'s, {timeout});
+    return await frame.contentFrame().$(selector);
+}
 
 async function waitForReadyPage(page, timeout) {
     let element = await Promise.race([
@@ -54,7 +13,8 @@ async function waitForReadyPage(page, timeout) {
             const elements = Array.from(document.querySelectorAll('td'));
             return elements.find(e => e.innerText.includes('You have been temporarily blocked to prevent unauthorized use. you have reached the maximum allowed connections, please try later.'));
         }, { timeout }),
-        waitForElementInFrame(page, '.captcha__human__title', timeout)
+        // <div class="captcha__human__title" tabindex="0">We want to make sure it is actually you we are dealing with and not a robot.</div>
+        getCaptchaMessage(page, '.captcha__human__title', timeout)
     ]);
 
     let message;
